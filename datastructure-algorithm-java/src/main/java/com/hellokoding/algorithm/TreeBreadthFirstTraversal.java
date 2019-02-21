1202cd1d04b8fae91bc98f91d60c99bd8e6c7350
Giau Ngo <giaunv@users.noreply.github.com>
2019-02-21 23:41:18 +0800

    Tree

---
+++ datastructure-algorithm-java/src/main/java/com/hellokoding/algorithm/TreeBreadthFirstTraversal.java
@@ -0,0 +1,35 @@
+package com.hellokoding.algorithm;
+
+import com.hellokoding.datastructure.BSTByLinkedList;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class TreeBreadthFirstTraversal extends BSTByLinkedList {
+    void breathFirstTraversal(Node root) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            System.out.println(currentNode);
+
+            if (currentNode.left != null)
+                queue.offer(currentNode.left);
+
+            if (currentNode.right != null)
+                queue.offer(currentNode.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeBreadthFirstTraversal tree = new TreeBreadthFirstTraversal();
+        tree.insert(7);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(1);
+        tree.insert(9);
+
+        tree.breathFirstTraversal(tree.root);
+    }
+}
