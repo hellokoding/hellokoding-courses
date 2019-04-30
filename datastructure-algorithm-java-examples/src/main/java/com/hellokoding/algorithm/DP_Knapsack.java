9c50e2f861cdb944fc1d01b04656a8225bfc3ab8
Giau Ngo <giaunv@users.noreply.github.com>
2019-04-30 22:54:48 +0800

    Knapsack 0-1

---
+++ datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/DP_Knapsack.java
@@ -0,0 +1,31 @@
+package com.hellokoding.algorithm;
+
+public class DP_Knapsack {
+    int findMaxValueOfKnapSack(int[] values, int[] weights, int W, int N) {
+        int[][] cache = new int[N+1][W+1];
+
+        for (int i = 0; i <= N; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    cache[i][w] = 0;
+                }
+                else if (weights[i-1] <= w) {
+                    cache[i][w] = Math.max(cache[i-1][w], cache[i-1][w-weights[i-1]] + values[i-1]);
+                } else {
+                    cache[i][w] = cache[i-1][w];
+                }
+            }
+        }
+
+        return cache[N][W];
+    }
+
+    public static void main(String[] args) {
+        DP_Knapsack knapsack = new DP_Knapsack();
+        int[] values = {60, 100, 120};
+        int[] weights = {10, 20, 30};
+        int weightLimit = 50;
+        int noOfItems = values.length;
+        System.out.println(knapsack.findMaxValueOfKnapSack(values, weights, weightLimit, noOfItems));
+    }
+}
