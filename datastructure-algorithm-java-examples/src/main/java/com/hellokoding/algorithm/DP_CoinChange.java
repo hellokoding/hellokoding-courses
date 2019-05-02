443a283397faa741e7d50430f55b325356209736
Giau Ngo <giaunv@users.noreply.github.com>
2019-05-02 23:39:39 +0800

    Coin change

---
+++ datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/DP_CoinChange.java
@@ -0,0 +1,21 @@
+package com.hellokoding.algorithm;
+
+public class DP_CoinChange {
+    static int countWays(int[] coins, int targetCoinChange) {
+        int[] wayOfCoinChanges = new int[targetCoinChange+1];
+
+        wayOfCoinChanges[0] = 1;
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = coins[i]; j <= targetCoinChange; j++) {
+                wayOfCoinChanges[j] += wayOfCoinChanges[j - coins[i]];
+            }
+        }
+
+        return wayOfCoinChanges[targetCoinChange];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countWays(new int[]{2, 3, 1}, 4));
+    }
+}
