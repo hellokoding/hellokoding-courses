5ff27898fd131bd393b05ee11b2a83a731dcb74c
Giau Ngo <giaunv@users.noreply.github.com>
2019-06-05 13:53:08 +0700

    Reverse Words

--- datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/String_ReverseWords.java
+++ datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/String_ReverseWords.java
@@ -2,13 +2,13 @@ package com.hellokoding.algorithm;
 
 public class String_ReverseWords {
     static String reverseWords(String str) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         String[] words = str.split(" ");
         for (int i = words.length - 1; i >= 0; i--) {
-            result += words[i] + " ";
+            result.append(words[i]).append(" ");
         }
 
-        return result;
+        return result.toString();
     }
 
     public static void main(String[] args) {
