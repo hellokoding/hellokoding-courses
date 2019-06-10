ac444a3180890d5018edfd7ec78770cc2894652e
Giau Ngo <giaunv@users.noreply.github.com>
2019-06-10 10:45:43 +0700

    Longest Common Substring

--- datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/String_LongestCommon.java
+++ datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/String_LongestCommon.java
@@ -5,8 +5,8 @@ public class String_LongestCommon {
         int m = S.length();
         int n = T.length();
         int[][] lengths = new int[m+1][n+1];
-        int z = 0;
-        String result = "";
+        int maxLength = 0;
+        int endIndex = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -15,15 +15,15 @@ public class String_LongestCommon {
                 }
                 else if(S.charAt(i-1) == T.charAt(j-1)) {
                     lengths[i][j] = lengths[i-1][j-1] + 1;
-                    z = Math.max(z, lengths[i][j]);
-                    result = S.substring(i-z+1, i+1);
+                    maxLength = Math.max(maxLength, lengths[i][j]);
+                    endIndex = i;
                 } else {
                     lengths[i][j] = 0;
                 }
             }
         }
 
-        return result;
+        return S.substring(endIndex - maxLength + 1, endIndex + 1);
     }
 
     public static void main(String[] args) {
