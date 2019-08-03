6412d41b8ec5326acfb409d6b43731cebdebf1f9
Giau Ngo <giaunv@users.noreply.github.com>
2019-08-03 19:18:33 +0700

    String pool

--- java-examples/java-core/src/test/java/com/hellokoding/java/lang/StringPoolTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/lang/StringPoolTest.java
@@ -13,7 +13,7 @@ public class StringPoolTest {
         // This string is reused from the string pool
         String s2 = "Hello Koding";
 
-        assertThat(s1 == s2).isTrue();
+        assertThat(s1).isSameAs(s2);
     }
 
     @Test
@@ -24,7 +24,7 @@ public class StringPoolTest {
         // This string is created and stored in heap memory
         String s2 = new String("Hello Koding");
 
-        assertThat(s1 == s2).isFalse();
+        assertThat(s1).isNotSameAs(s2);
     }
 
     @Test
@@ -35,7 +35,7 @@ public class StringPoolTest {
         // This string is created and stored in the string pool
         String s2 = "Hello Koding";
 
-        assertThat(s1 == s2).isFalse();
+        assertThat(s1).isNotSameAs(s2);
     }
 
     @Test
@@ -45,6 +45,6 @@ public class StringPoolTest {
 
         String s2 = "Hello Koding";
 
-        assertThat(s1 == s2).isTrue();
+        assertThat(s1).isSameAs(s2);
     }
 }
