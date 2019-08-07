1e53cbeadf70c0c491fc729c3859f489f11e5432
Giau Ngo <giaunv@users.noreply.github.com>
2019-08-08 00:03:46 +0800

    Docker compose spring boot mongodb

--- docker-examples/dockercompose-springboot-mongodb-nginx/app/src/main/java/com/hellokoding/springboot/IndexController.java
+++ docker-examples/dockercompose-springboot-mongodb-nginx/app/src/main/java/com/hellokoding/springboot/IndexController.java
@@ -1,23 +1,26 @@
 package com.hellokoding.springboot;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Controller
+@RequestMapping("/")
 public class IndexController {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
-    @GetMapping("/")
+    @Autowired
+    MongoClient mongoClient;
+
+    @GetMapping
     public String index(Model model) {
-        try(MongoClient mongoClient = new MongoClient("mongodb")){
-            logger.log(Level.INFO, "First database name: " + mongoClient.listDatabaseNames().first());
-        }
-        
+        logger.log(Level.INFO, "First database name: {0}", mongoClient.listDatabaseNames().first());
         return "index";
     }
 }
--- docker-examples/dockercompose-springboot-mongodb-nginx/app/src/main/java/com/hellokoding/springboot/WebApplication.java
+++ docker-examples/dockercompose-springboot-mongodb-nginx/app/src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -2,6 +2,7 @@ package com.hellokoding.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class WebApplication {
