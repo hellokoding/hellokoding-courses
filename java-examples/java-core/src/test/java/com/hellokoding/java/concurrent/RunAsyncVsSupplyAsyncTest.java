f7d421d41ed9f9761170d418d49fca51f1de2977
Giau Ngo <giaunv@users.noreply.github.com>
2019-09-10 12:35:16 +0800

    Java RunAsync vs SupplyAsync

--- java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/RunAsyncVsSupplyAsyncTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/RunAsyncVsSupplyAsyncTest.java
@@ -2,64 +2,64 @@ package com.hellokoding.java.concurrent;
 
 import org.junit.Test;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.hellokoding.java.concurrent.ConcurrentUtils.stop;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RunAsyncVsSupplyAsyncTest {
-    private ExecutorService executorService = Executors.newFixedThreadPool(3);
+    private ExecutorService executorService = Executors.newFixedThreadPool(2);
 
     @Test
-    public void runAsync() {
-        Counter counter = new Counter();
+    public void runAsync() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture.runAsync(() -> System.out.println("runAsync has no return values"));
 
-        for (int i = 0; i < 10000; i++) {
-            CompletableFuture.runAsync(counter::increaseRunnable);
-        }
-
-        stop(ForkJoinPool.commonPool());
-
-        assertThat(counter.count).isEqualTo(10000);
+        assertThat(c.get()).isNull();
     }
 
     @Test
-    public void runAsyncWithExecutorService() {
-        Counter counter = new Counter();
+    public void runAsyncWithCallbacks() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture
+            .runAsync(() -> System.out.println("runAsync"))
+            .thenRunAsync(() -> System.out.println("callback"));
 
-        for (int i = 0; i < 10000; i++) {
-            CompletableFuture.runAsync(counter::increaseRunnable, executorService);
-        }
+        assertThat(c.get()).isNull();
+    }
+
+    @Test
+    public void runAsyncWithExecutor() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture.runAsync(() -> System.out.println("Run runAsync with an Executor"), executorService);
 
         stop(executorService);
 
-        assertThat(counter.count).isEqualTo(10000);
+        assertThat(c.get()).isNull();
     }
 
     @Test
-    public void supplyAsync() {
-        Counter counter = new Counter();
+    public void supplyAsync() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture.supplyAsync(() -> "supplyAsync has return value");
 
-        for (int i = 0; i < 10000; i++) {
-            CompletableFuture.supplyAsync(counter::increaseSupplier, executorService);
-        }
+        assertThat(c.get()).isEqualTo("supplyAsync has return value");
+    }
+
+    @Test
+    public void supplyAsyncWithCallbacks() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture
+            .supplyAsync(() -> "supplyAsync")
+            .thenApplyAsync((s) -> s + " callback");
+
+        assertThat(c.get()).isEqualTo("supplyAsync callback");
+    }
+
+    @Test
+    public void supplyAsyncWithExecutor() throws ExecutionException, InterruptedException {
+        CompletableFuture c = CompletableFuture.supplyAsync(() -> "run supplyAsync with an Executor", executorService);
 
         stop(executorService);
 
-        assertThat(counter.count).isEqualTo(10000);
-    }
-}
-
-class Counter {
-    int count = 0;
-
-    synchronized void increaseRunnable() {
-        count++;
-    }
-
-    synchronized int increaseSupplier() {
-        count++;
-
-        return count;
+        assertThat(c.get()).isEqualTo("run supplyAsync with an Executor");
     }
 }
