61cc004423dc337c5a49bac04374d405b5657967
Giau Ngo <giaunv@users.noreply.github.com>
2019-09-19 00:12:13 +0800

    Java Concurrency

--- java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/SupplyAsyncTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/SupplyAsyncTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
+import static com.hellokoding.java.concurrent.ConcurrentUtils.stop;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SupplyAsyncTest {
@@ -18,30 +21,42 @@ public class SupplyAsyncTest {
     }
 
     @Test
-    public void thenApply() throws ExecutionException, InterruptedException {
+    public void thenApplyAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Future");
 
         completableFuture = completableFuture.thenApplyAsync((s) -> s.concat(" is awesome!"));
-        completableFuture = completableFuture.thenApply((s) -> s.concat("!"));
 
-        assertThat(completableFuture.get()).isEqualTo("Future is awesome!!");
+        assertThat(completableFuture.get()).isEqualTo("Future is awesome!");
     }
 
     @Test
-    public void thenCompose() throws ExecutionException, InterruptedException {
+    public void thenApplyAsyncWithExecutor() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Future", executorService);
+
+        completableFuture = completableFuture.thenApplyAsync((s) -> s.concat(" is awesome!"), executorService);
+
+        stop(executorService);
+
+        assertThat(completableFuture.get()).isEqualTo("Future is awesome!");
+    }
+
+    @Test
+    public void thenComposeAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<String> composedCompletableFuture = CompletableFuture
                 .supplyAsync(() -> "Future")
-                .thenCompose(s -> CompletableFuture.supplyAsync(() -> s.concat(" is awesome!")));
+                .thenComposeAsync(s -> CompletableFuture.supplyAsync(() -> s.concat(" is awesome!")));
 
         assertThat(composedCompletableFuture.get()).isEqualTo("Future is awesome!");
     }
 
     @Test
-    public void thenCombine() throws ExecutionException, InterruptedException {
+    public void thenCombineAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> "Future");
         CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> " is awesome!");
 
-        CompletableFuture<String> combinedCompletableFuture = completableFuture1.thenCombine(completableFuture2, (s1, s2) -> s1.concat(s2));
+        CompletableFuture<String> combinedCompletableFuture = completableFuture1.thenCombineAsync(completableFuture2, (s1, s2) -> s1.concat(s2));
 
         assertThat(combinedCompletableFuture.get()).isEqualTo("Future is awesome!");
     }
@@ -73,23 +88,22 @@ public class SupplyAsyncTest {
     }
 
     @Test
-    public void thenAccept() throws ExecutionException, InterruptedException {
+    public void thenAcceptAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Future");
 
         completableFuture = completableFuture.thenApplyAsync((s) -> s.concat(" is awesome!"));
-        CompletableFuture<Void> procedureFuture = completableFuture.thenAccept(System.out::println);
+        CompletableFuture<Void> procedureFuture = completableFuture.thenAcceptAsync(System.out::println);
 
         assertThat(procedureFuture.get()).isNull();
     }
 
     @Test
-    public void thenRun() throws ExecutionException, InterruptedException {
+    public void thenRunAsync() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "Future");
 
         completableFuture = completableFuture.thenApplyAsync((s) -> s.concat(" is awesome!"));
-        CompletableFuture<Void> procedureFuture = completableFuture.thenRun(() -> System.out.println("!"));
+        CompletableFuture<Void> procedureFuture = completableFuture.thenRunAsync(() -> System.out.println("!"));
 
-        assertThat(procedureFuture.isDone()).isTrue();
         assertThat(procedureFuture.get()).isNull();
     }
 }
