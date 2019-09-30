1d82d4a33c6ac0b6e92cff300d9602047a672e5f
Giau Ngo <giaunv@users.noreply.github.com>
2019-09-30 23:19:35 +0800

    Java HashSet

---
+++ java-examples/java-core/src/test/java/com/hellokoding/java/collections/HashSetSortingTest.java
@@ -0,0 +1,45 @@
+package com.hellokoding.java.collections;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HashSetSortingTest {
+    @Test
+    public void sortAscWithTreeSet() {
+        Set<Integer> set = new HashSet<>(Set.of(3, 1, 2));
+        NavigableSet<Integer> sortedSet = new TreeSet<>(set);
+
+        // Asserts that the set contains the given values in order
+        assertThat(sortedSet).containsExactly(1, 2, 3);
+    }
+
+    @Test
+    public void sortDescWithTreeSet() {
+        Set<Integer> set = new HashSet<>(Set.of(3, 1, 2));
+        NavigableSet<Integer> sortedSet = new TreeSet<>(set);
+        NavigableSet<Integer> descSet = sortedSet.descendingSet();
+
+        assertThat(descSet).containsExactly(3, 2, 1);
+    }
+
+    @Test
+    public void sortAscWithArrayList() {
+        Set<String> set = new HashSet<>(Set.of("c", "a", "b"));
+        List<String> list = new ArrayList<>(set);
+        list.sort(Comparator.naturalOrder());
+
+        assertThat(list).containsExactly("a", "b", "c");
+    }
+
+    @Test
+    public void sortDescWithArrayList() {
+        Set<String> set = new HashSet<>(Set.of("c", "a", "b"));
+        List<String> list = new ArrayList<>(set);
+        list.sort(Comparator.reverseOrder());
+
+        assertThat(list).containsExactly("c", "b", "a");
+    }
+}
