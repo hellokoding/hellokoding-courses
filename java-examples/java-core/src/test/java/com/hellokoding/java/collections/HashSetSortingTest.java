bf6f670a1203236d42d8e3e200cea4da1e10ad5f
Giau Ngo <giaunv@users.noreply.github.com>
2019-10-03 23:54:07 +0800

    Sort HashSet

--- java-examples/java-core/src/test/java/com/hellokoding/java/collections/HashSetSortingTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/collections/HashSetSortingTest.java
@@ -8,47 +8,84 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class HashSetSortingTest {
     @Test
-    public void sortAscWithTreeSet() {
+    public void sortWith_TreeSet_And_Comparable() {
         Set<Integer> set = new HashSet<>(Set.of(3, 1, 2));
-        NavigableSet<Integer> sortedSet = new TreeSet<>(set);
 
-        // Asserts that the set contains the given values in order
+        NavigableSet<Integer> sortedSet = new TreeSet<>(set);
         assertThat(sortedSet).containsExactly(1, 2, 3);
-    }
 
-    @Test
-    public void sortDescWithTreeSet() {
-        Set<Integer> set = new HashSet<>(Set.of(3, 1, 2));
-        NavigableSet<Integer> sortedSet = new TreeSet<>(set);
         NavigableSet<Integer> descSet = sortedSet.descendingSet();
-
         assertThat(descSet).containsExactly(3, 2, 1);
     }
 
     @Test
-    public void sortDescWithTreeSet2() {
+    public void sortWith_TreeSet_And_Comparator() {
         Set<Integer> set = new HashSet<>(Set.of(3, 1, 2));
+
         NavigableSet<Integer> sortedSet = new TreeSet<>(Comparator.reverseOrder());
         sortedSet.addAll(set);
-
         assertThat(sortedSet).containsExactly(3, 2, 1);
     }
 
     @Test
-    public void sortAscWithArrayList() {
-        Set<String> set = new HashSet<>(Set.of("c", "a", "b"));
-        List<String> list = new ArrayList<>(set);
-        list.sort(Comparator.naturalOrder());
+    public void sortWith_TreeSet_And_CustomComparator() {
+        Book book1 = new Book(1, "b");
+        Book book2 = new Book(2, "c");
+        Book book3 = new Book(3, "c");
+        Set<Book> set = new HashSet<>(Set.of(book1, book2, book3));
 
-        assertThat(list).containsExactly("a", "b", "c");
+        Comparator<Book> c = Comparator
+            .comparing(Book::getTitle, Comparator.reverseOrder())
+            .thenComparing(Book::getId, Comparator.reverseOrder());
+
+        NavigableSet<Book> sortedSet = new TreeSet<>(c);
+        sortedSet.addAll(set);
+
+        assertThat(sortedSet).containsExactly(book3, book2, book1);
     }
 
     @Test
-    public void sortDescWithArrayList() {
+    public void sortWith_ArrayList_And_Comparable() {
         Set<String> set = new HashSet<>(Set.of("c", "a", "b"));
         List<String> list = new ArrayList<>(set);
-        list.sort(Comparator.reverseOrder());
 
+        list.sort(Comparator.naturalOrder());
+        assertThat(list).containsExactly("a", "b", "c");
+
+        list.sort(Comparator.reverseOrder());
         assertThat(list).containsExactly("c", "b", "a");
+    }
+
+    @Test
+    public void sortWith_ArrayList_And_CustomComparator() {
+        Book book1 = new Book(1, "b");
+        Book book2 = new Book(2, "c");
+        Book book3 = new Book(3, "c");
+        Set<Book> set = new HashSet<>(Set.of(book1, book2, book3));
+
+        List<Book> list = new ArrayList<>(set);
+        list.sort(Comparator
+            .comparing(Book::getTitle, Comparator.reverseOrder())
+            .thenComparing(Book::getId, Comparator.reverseOrder()));
+
+        assertThat(list).containsExactly(book3, book2, book1);
+    }
+
+    static class Book {
+        int id;
+        String title;
+
+        Book(int id, String title) {
+            this.id = id;
+            this.title = title;
+        }
+
+        int getId() {
+            return id;
+        }
+
+        String getTitle() {
+            return title;
+        }
     }
 }
