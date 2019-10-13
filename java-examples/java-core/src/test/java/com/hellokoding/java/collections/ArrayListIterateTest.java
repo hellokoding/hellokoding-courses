6351fb2b2cb5f6de9ead8e64ea12ab29ff817c03
Giau Ngo <giaunv@users.noreply.github.com>
2019-10-13 23:17:49 +0800

    Java ArrayList

--- java-examples/java-core/src/test/java/com/hellokoding/java/collections/ArrayListIterateTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/collections/ArrayListIterateTest.java
@@ -5,53 +5,62 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ArrayListIterateTest {
     @Test
-    public void forIndex() {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
-
-        for(int i=0; i<lst.size(); i++){
-            int ele = lst.get(i);
-            System.out.println(ele);
-        }
-    }
-
-    @Test
-    public void forEach() {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
-
-        for(int ele : lst){
-            System.out.println(ele);
-        }
-    }
-
-    @Test
     public void forEachConsumer() {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
 
         lst.forEach(e -> System.out.printf("%d ", e));
     }
 
     @Test
+    public void forEach() {
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
+
+        for(int ele : lst){
+            System.out.printf("%d ", ele);
+        }
+    }
+
+    @Test
+    public void forIndexForward() {
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
+
+        for(int i=0; i<lst.size(); i++){
+            int ele = lst.get(i);
+            System.out.printf("%d ", ele);
+        }
+    }
+
+    @Test
+    public void forIndexBackward() {
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
+
+        for(int i=lst.size()-1; i>=0; i--){
+            int ele = lst.get(i);
+            System.out.printf("%d ", ele);
+        }
+    }
+
+    @Test
     public void iterator() {
-        List<Integer> lst = new ArrayList<>();
-        lst.add(1);
-        lst.add(2);
-        lst.add(3);
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
 
         Iterator<Integer> iterator = lst.iterator();
         while (iterator.hasNext()){
-            System.out.println(iterator.next());
+            System.out.printf("%d ", iterator.next());
+        }
+    }
+
+    @Test
+    public void listIterator() {
+        List<Integer> lst = new ArrayList<>(List.of(1, 2, 3));
+
+        ListIterator<Integer> iterator = lst.listIterator(lst.size());
+        while (iterator.hasPrevious()){
+            System.out.printf("%d ", iterator.previous());
         }
     }
 }
