d8f3526236e7a567db045b4b93a719468105f243
Giau Ngo <giaunv@users.noreply.github.com>
2020-01-17 20:22:32 +0800

    Spring Boot JSP

--- springboot-examples/springboot-jsp-helloworld/src/main/java/com/hellokoding/springboot/view/Application.java
+++ springboot-examples/springboot-jsp-helloworld/src/main/java/com/hellokoding/springboot/view/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
