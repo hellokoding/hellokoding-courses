fca132405d9f2fe2bddd4cb7d99634cda8ca4c40
Giau Ngo <giaunv@users.noreply.github.com>
2020-01-17 20:37:25 +0800

    Spring Boot JSP

--- springboot-examples/springboot-jsp-helloworld/src/main/java/com/hellokoding/springboot/view/Application.java
+++ springboot-examples/springboot-jsp-helloworld/src/main/java/com/hellokoding/springboot/view/Application.java
@@ -2,8 +2,6 @@ package com.hellokoding.springboot.view;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application {
