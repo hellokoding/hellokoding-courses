c89a63021c61686b6a26ad7eba1d61570bf2cd99
Giau Ngo <giaunv@users.noreply.github.com>
2020-02-05 23:18:21 +0800

    Exception handling in Java CompletableFuture

--- java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/CompletableFutureExceptionHandlingTest.java
+++ java-examples/java-core/src/test/java/com/hellokoding/java/concurrent/CompletableFutureExceptionHandlingTest.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
-
 public class CompletableFutureExceptionHandlingTest {
     @Test
-    public void handle() {
+    public void handleWithException() {
         CompletableFuture completableFuture = CompletableFuture
             .supplyAsync(() -> Integer.parseInt("s"))
             .handleAsync((result, e) -> {
@@ -16,39 +14,66 @@ public class CompletableFutureExceptionHandlingTest {
                     System.out.println(e.getMessage());
                     return "Error!";
                 } else {
-                    return result;
+                    return result + 1;
                 }
             })
             .thenAcceptAsync(System.out::println);
-
-        assertThat(completableFuture.join())
-            .isNull();
     }
 
     @Test
-    public void exceptionally() {
+    public void handleWithoutExceptions() {
+        CompletableFuture completableFuture = CompletableFuture
+            .supplyAsync(() -> 1)
+            .handleAsync((result, e) -> {
+                if (e != null) {
+                    System.out.println(e.getMessage());
+                    return "Error!";
+                } else {
+                    return result + 1;
+                }
+            })
+            .thenAcceptAsync(System.out::println);
+    }
+
+    @Test
+    public void exceptionallyWithException() {
         CompletableFuture completableFuture = CompletableFuture
             .supplyAsync(() -> Integer.parseInt("s"))
             .exceptionallyAsync((e) -> {
                 System.out.println(e.getMessage());
-                return null;
+                return 0;
             })
             .thenAcceptAsync(System.out::println);
-
-        assertThat(completableFuture.join())
-            .isNull();
     }
 
     @Test
-    public void whenComplete() {
+    public void exceptionallyWithoutExceptions() {
+        CompletableFuture completableFuture = CompletableFuture
+            .supplyAsync(() -> 1)
+            .exceptionallyAsync((e) -> {
+                System.out.println(e.getMessage());
+                return 0;
+            })
+            .thenAcceptAsync(System.out::println);
+    }
+
+    @Test
+    public void whenCompleteWithException() {
         CompletableFuture completableFuture = CompletableFuture
             .supplyAsync(() -> Integer.parseInt("s"))
             .whenCompleteAsync((result, e) -> {
                 if (e != null) System.out.println(e.getMessage());
             })
             .thenAcceptAsync(System.out::println);
+    }
 
-        assertThat(completableFuture.join())
-            .isNull();
+    @Test
+    public void whenCompleteWithoutExceptions() {
+        CompletableFuture completableFuture = CompletableFuture
+            .supplyAsync(() -> 1)
+            .whenCompleteAsync((result, e) -> {
+                if (e != null) System.out.println(e.getMessage());
+            })
+            .thenAcceptAsync(System.out::println);
     }
 }
