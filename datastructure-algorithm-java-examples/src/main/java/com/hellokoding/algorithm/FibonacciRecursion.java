d4bf228bdff36f32e2603fd32a3a86002cd80b53
rmukalla <62635319+rmukalla@users.noreply.github.com>
2020-04-23 14:37:09 -0700

    Update FibonacciRecursion.java
    Testing

--- datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/FibonacciRecursion.java
+++ datastructure-algorithm-java-examples/src/main/java/com/hellokoding/algorithm/FibonacciRecursion.java
@@ -1,9 +1,9 @@
 package com.hellokoding.algorithm;
 
 public class FibonacciRecursion {
-    int fibonacci(int i) {
-        if (i < 2) return i;
-        return fibonacci(i-1) + fibonacci(i-2);
+    int fibonacci(int j) {
+        if (j < 2) return i;
+        return fibonacci(j-1) + fibonacci(j-2);
     }
 
     public static void main(String[] args) {
